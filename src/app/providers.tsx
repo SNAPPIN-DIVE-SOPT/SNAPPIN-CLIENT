add35dd36667801bab30902c09f513baa4c0b749
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-12-29 15:12:35 +0900

    feat: 전체적인 layout 구현

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,3 +1,16 @@
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <div
+      className="
+    bg-white
+    shadow-[0_0_10px_4px_rgba(0,0,0,0.04)]
+    max-w-[45rem]
+    mx-auto
+    h-screen
+    overflow-y-auto
+  "
+    >
+      {children}
+    </div>
+  );
 }
