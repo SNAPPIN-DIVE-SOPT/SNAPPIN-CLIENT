307eca24bf9f9808132948b7a4e103000f8d2ff3
bhyerain <seunghye0307@naver.com>
2026-01-02 00:37:31 +0900

    chore: 스토리북 내용 및 경로 수정

--- src/ui/section-tabs/SectionTabs.stories.tsx
+++ src/ui/section-tabs/SectionTabs.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import { useState } from 'react';
 
 import SectionTabs from './SectionTabs';
@@ -7,8 +7,14 @@ import { SECTION_TAB, SECTION_TAB_PRESET, getSectionTabLabel } from './types/sec
 const meta: Meta<typeof SectionTabs> = {
   title: 'UI/SectionTabs',
   component: SectionTabs,
+  parameters: {
+    docs: {
+      description: {
+        component: '페이지별로 다른 섹션 구성을 가질 수 있는 공용 탭 컴포넌트입니다.',
+      },
+    },
+  },
 };
-
 export default meta;
 type Story = StoryObj<typeof SectionTabs>;
 
@@ -33,40 +39,12 @@ function SectionTabsTemplate({
 
 export const ClientTab: Story = {
   render: () => <SectionTabsTemplate tabs={SECTION_TAB_PRESET.CLIENT_TAB} />,
-  parameters: {
-    docs: {
-      description: {
-        story:
-          '클라이언트 홈에서 사용하는 기본 섹션 탭입니다.\n' +
-          '포트폴리오 / 상품 탭으로 구성되며, 최소 탭 개수(2개) 사용 예시입니다.',
-      },
-    },
-  },
 };
 
 export const DetailTabWithReviewCount: Story = {
   render: () => <SectionTabsTemplate tabs={SECTION_TAB_PRESET.DETAIL_TAB} reviewCount={12} />,
-  parameters: {
-    docs: {
-      description: {
-        story:
-          '상품 상세 페이지에서 사용하는 섹션 탭입니다.\n' +
-          '리뷰 탭에는 리뷰 개수가 동적으로 표시되며,\n' +
-          '`getLabel` 함수를 통해 외부 데이터가 라벨에 반영되는 예시입니다.',
-      },
-    },
-  },
 };
 
 export const AuthorReservation: Story = {
   render: () => <SectionTabsTemplate tabs={SECTION_TAB_PRESET.AUTHOR_RESERVATION} />,
-  parameters: {
-    docs: {
-      description: {
-        story:
-          '작가 예약 관리 페이지(와프 참고)에서 사용하는 섹션 탭입니다.\n' +
-          '4개 이상의 탭을 사용하는 대표적인 예시입니다.',
-      },
-    },
-  },
 };
