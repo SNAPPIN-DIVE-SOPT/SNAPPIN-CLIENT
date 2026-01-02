7f55e764f0e4098a9026bbd537ce1deaedf7aadf
bhyerain <seunghye0307@naver.com>
2026-01-02 21:48:34 +0900

    refactor: section-tabs 인덱스 단순화

--- src/ui/section-tabs/index.ts
+++ src/ui/section-tabs/index.ts
@@ -1,10 +1,1 @@
 export { default as SectionTabs } from './SectionTabs';
-
-export {
-  SECTION_TAB,
-  SECTION_TAB_LABEL,
-  SECTION_TAB_PRESET,
-  getSectionTabLabel,
-} from './constants/sectionTabTheme';
-
-export type { SectionTabsProps } from './types/sectionLabel';
