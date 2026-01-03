0b8fb28e8e7eadedbc26ce06b2038fa8ac2332d2
민하 <sminha8590@naver.com>
2026-01-04 01:32:15 +0900

    refactor: 불필요 css 제거

--- src/ui/toggle/Toggle.tsx
+++ src/ui/toggle/Toggle.tsx
@@ -24,11 +24,11 @@ export default function Toggle({
       {...props}
     >
       <div className={cn(
-        'flex flex-col shrink-0 gap-[1rem] w-[7.7rem] h-[3.2rem] p-[0.3rem] rounded-[2.4rem] transition-[transform_background-color] duration-500 ease-in-out',
+        'flex flex-col shrink-0 w-[7.7rem] h-[3.2rem] p-[0.3rem] rounded-[2.4rem] transition-[transform_background-color] duration-500 ease-in-out',
         trackClassName
       )}>
         <div className={cn(
-          'flex shrink-0 justify-center items-center gap-[1rem] w-[4.4rem] h-[2.5rem] px-[1.3rem] py-[0.8rem] rounded-[1.9rem] transition-[transform_background-color] duration-500 ease-in-out',
+          'flex shrink-0 justify-center items-center w-[4.4rem] h-[2.5rem] px-[1.3rem] py-[0.8rem] rounded-[1.9rem] transition-[transform_background-color] duration-500 ease-in-out',
           thumbClassName
         )}>
           <span className={cn('caption-12-md transition-[color] duration-500 ease-in-out', labelClassName)}>
