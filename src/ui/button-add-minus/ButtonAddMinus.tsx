289152e68171800b5e6503c3d0e45201a0080820
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-03 10:02:13 +0900

    feat: button-add-minus component

---
+++ src/ui/button-add-minus/ButtonAddMinus.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { IconAdd, IconRemove } from '@/assets';
+import { cn } from '@/utils/cn';
+
+type ButtonAddMinusProps = {
+  value: string | number;
+  handleClickMinus: () => void;
+  handleClickAdd: () => void;
+  isDisabledMinus?: boolean;
+  isDisabledAdd?: boolean;
+  className?: string;
+};
+
+export default function ButtonAddMinus({
+  value,
+  handleClickMinus,
+  handleClickAdd,
+  isDisabledMinus,
+  isDisabledAdd,
+  className,
+}: ButtonAddMinusProps) {
+  return (
+    <div
+      className={cn(
+        'bg-black-3 flex h-[3.6rem] w-fit items-center gap-[1rem] rounded-[0.4rem] px-[0.8rem] py-[0.3rem]',
+        className,
+      )}
+    >
+      <IconRemove
+        onClick={isDisabledMinus ? undefined : handleClickMinus}
+        className={cn('cursor-pointer', isDisabledMinus && 'cursor-not-allowed')}
+      />
+      <span className='bg-black-1 font-16-md flex h-[3rem] items-center justify-center px-[1.8rem] py-[0.2rem] text-center'>
+        {value}
+      </span>
+      <IconAdd
+        onClick={isDisabledAdd ? undefined : handleClickAdd}
+        className={cn('cursor-pointer', isDisabledAdd && 'cursor-not-allowed')}
+      />
+    </div>
+  );
+}
