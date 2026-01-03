8436840c887a3151bff706c4c206a412ec29ea9f
Tnalxmsk <alstnwkd990@naver.com>
2026-01-03 23:13:06 +0900

    fix: 잘못된 컬러 값 수정

--- src/ui/divider/constants/theme.ts
+++ src/ui/divider/constants/theme.ts
@@ -2,5 +2,5 @@ import { DividerColor } from '@/ui/divider/type/variant';
 
 export const DIVIDER_COLOR: Record<NonNullable<DividerColor>, string> = {
   'gray-dark': 'bg-black-5',
-  'gray-light': 'bg-gray-3',
+  'gray-light': 'bg-black-3',
 };
