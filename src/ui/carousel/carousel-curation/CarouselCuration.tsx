e6b21fccebf1cbf2325ca877d7a31ebfe41b72a0
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-05 20:30:03 +0900

    refactor: 스토리 경로 수정

--- src/ui/carousel/carousel-curation/CarouselCuration.tsx
+++ src/ui/carousel/carousel-curation/CarouselCuration.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import Image from 'next/image';
 
 import { cn } from '@/utils/cn';
 import {
@@ -10,6 +9,7 @@ import {
   CarouselItem,
   type CarouselApi,
 } from '@/ui/carousel/base/Carousel';
+import { ImageCarousel } from '@/ui/carousel';
 import { IconEllipse } from '@/assets';
 import { TagChip } from '@/ui/chip';
 import { TagCode } from '@/ui/chip/tag-chip/types/tagCode';
@@ -48,23 +48,14 @@ export default function CarouselCuration({ images, tags, name, className }: Caro
         <CarouselContent>
           {images.map((img, idx) => (
             <CarouselItem key={idx}>
-              <div className='relative h-[36rem] w-[36rem]'>
-                <Image
-                  src={img.src}
-                  alt={img.alt ?? `image-${img.src}`}
-                  fill
-                  sizes='36rem'
-                  className='object-cover select-none'
-                  priority={idx === 0}
-                  draggable={false}
-                />
-                <div
-                  className='pointer-events-none absolute inset-0 h-full w-full'
-                  style={{
-                    background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 46.63%, #000 100%)',
-                  }}
-                />
-              </div>
+              <ImageCarousel
+                src={img.src}
+                alt={img.alt ?? `image-${img.src}`}
+                imageHeight='36rem'
+                imageWidth='36rem'
+                priority={idx === 0}
+                draggable={false}
+              />
             </CarouselItem>
           ))}
         </CarouselContent>
