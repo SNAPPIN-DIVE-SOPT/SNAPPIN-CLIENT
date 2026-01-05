dedab2f3ef0a0557504cccd48779f14345000870
민하 <sminha8590@naver.com>
2026-01-05 19:02:34 +0900

    refactor: 토끼리뷰 반영

--- src/ui/user-type-toggle/types/userType.ts
+++ src/ui/user-type-toggle/types/userType.ts
@@ -2,6 +2,6 @@ export const USER_TYPES = ['client', 'author'] as const;
 export const USER_TYPE_LABEL: Record<UserType, string> = {
   client: '고객',
   author: '작가'
-}
+};
 
 export type UserType = typeof USER_TYPES[number];
