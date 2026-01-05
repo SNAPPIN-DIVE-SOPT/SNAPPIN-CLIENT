9962498118c2d88cfe39d4c80aa1019c80c692c9
민하 <sminha8590@naver.com>
2026-01-06 03:39:43 +0900

    refactor: FilterChipStatus 네이밍 변경
    - filterChipStatus -> filterChipVariant
    - removable -> selectedRemovable

--- src/ui/chip/filter-chip/constants/filterChipTheme.ts
+++ src/ui/chip/filter-chip/constants/filterChipTheme.ts
@@ -1,9 +1,9 @@
-import { FilterChipStatus } from '../types/filterChipStatus';
+import { FilterChipVariant } from '../types/filterChipVariant';
 
 type FilterChipTheme = { buttonClassName: string; labelClassName: string; };
 
-export const FILTER_CHIP_THEME: Record<FilterChipStatus, FilterChipTheme> = {
+export const FILTER_CHIP_THEME: Record<FilterChipVariant, FilterChipTheme> = {
   'default' : { buttonClassName: 'px-[0.8rem] bg-black-3', labelClassName: 'text-black-8' },
   'selected': { buttonClassName: 'px-[0.8rem] bg-black-10', labelClassName: 'text-black-1' },
-  'removable': { buttonClassName: 'h-[2.6rem] pl-[0.8rem] pr-[0.3rem]', labelClassName: 'text-black-1 cursor-default' },
+  'selectedRemovable': { buttonClassName: 'h-[2.6rem] pl-[0.8rem] pr-[0.3rem]', labelClassName: 'text-black-1 cursor-default' },
 }
--- src/ui/chip/filter-chip/types/filterChipStatus.ts
+++
@@ -1,1 +0,0 @@
-export type FilterChipStatus = 'default' | 'selected' | 'removable';
