085045d7e31e1bc118c5ff5f21a5b3d39a13b238
bhyerain <seunghye0307@naver.com>
2026-01-06 03:37:51 +0900

    feat: SearchBar 컴포넌트 추가

---
+++ src/ui/search-field/SearchBar.tsx
@@ -0,0 +1,33 @@
+import { cn } from '@/utils/cn';
+import SearchFieldBase from './base/SearchField';
+
+type SearchBarProps = Omit<
+  React.ComponentProps<typeof SearchFieldBase>,
+  'headline' | 'supportingText'
+>;
+
+export default function SearchBar({
+  className,
+  containerClassName,
+  inputWrapperClassName,
+  iconWrapperClassName,
+  iconClassName,
+  ...props
+}: SearchBarProps) {
+  return (
+    <SearchFieldBase
+      containerClassName={cn(
+        'bg-black-3 relative flex w-full items-center gap-[0.5625rem] rounded-[4rem] px-[1.5rem] py-[1.15rem]',
+        containerClassName,
+      )}
+      iconWrapperClassName={cn('shrink-0', iconWrapperClassName)}
+      iconClassName={cn('text-black-9 h-[2.4rem] w-[2.4rem]', iconClassName)}
+      inputWrapperClassName={cn('flex-1', inputWrapperClassName)}
+      className={cn(
+        'text-black-10 caption-14-md placeholder-black-7 w-full bg-transparent focus:outline-none',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
