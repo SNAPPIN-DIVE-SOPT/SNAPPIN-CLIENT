3d7d62559082222578b4e6ade9390f66d4120572
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-06 16:44:24 +0900

    feat: [작가] 예약 페이지, header 구현

---
+++ src/app/author/(with-layout)/reservation/components/navigation-client/Navigation.client.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { Navigation } from '@/ui';
+import { Logo, IconSearch } from '@/assets';
+
+export default function NavigationClient() {
+  const handleSearchClick = () => {
+    //TODO: 검색 페이지로 이동
+  };
+
+  return <Navigation left={<Logo />} right={<IconSearch onClick={handleSearchClick} />} />;
+}
---
+++ src/app/author/(with-layout)/reservation/page.tsx
@@ -0,0 +1,10 @@
+import NavigationClient from './components/navigation-client/Navigation.client';
+
+export default function page() {
+  return (
+    <div>
+      <NavigationClient />
+      {/* TODO: tab */}
+    </div>
+  );
+}
