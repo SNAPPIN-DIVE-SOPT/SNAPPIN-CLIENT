375fc22569c8abf7aceb32b53a32aae930caeec6
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-07 16:53:48 +0900

    feat: 상태에 따른 버튼 로직 구현

--- src/app/author/reservation-detail/[id]/@section/ProductStatus.tsx
+++ src/app/author/reservation-detail/[id]/@section/ProductStatus.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import { ProductCard } from '@/ui/product-card';
 import { MoodCode } from '@/types/moodCode';
+import { StateCode } from '@/types/stateCode';
+import Button from '@/ui/button/base/Button';
 
 type ProductStatusProps = {
+  id: number;
   imageUrl: string;
   title: string;
   rate: number;
@@ -9,9 +14,11 @@ type ProductStatusProps = {
   photographer: string;
   price: number;
   moods: MoodCode[];
+  status: StateCode;
 };
 
 export default function ProductStatus({
+  id,
   imageUrl,
   title,
   rate,
@@ -19,11 +26,21 @@ export default function ProductStatus({
   photographer,
   price,
   moods,
+  status,
 }: ProductStatusProps) {
+  const handleRefuse = () => {
+    //TODO: 예약 거절 기능 구현
+    console.info('예약 거절', id);
+  };
+
+  const handleSendMessage = () => {
+    //TODO: 메시지 기능은 준비 중 이에요. 조금만 기다려주세요! -> toast 띄우기
+  };
+
   return (
-    <div className='bg-black-1 flex flex-col px-[2rem] pt-[1.7rem]'>
+    <div className='bg-black-1 flex flex-col px-[2rem] pt-[1.7rem] pb-[1.2rem]'>
       <p className='caption-14-bd'>예약 요청 상품</p>
-      <div className='w-full py-[1.2rem]'>
+      <div className='w-full pt-[1.2rem]'>
         <ProductCard
           image={{ src: imageUrl, alt: title }}
           name={title}
@@ -33,6 +50,27 @@ export default function ProductStatus({
           price={price}
           tags={moods}
         />
+      </div>
+      <div className='flex w-full items-center gap-[0.6rem] pt-[1.7rem]'>
+        {status !== 'RESERVATION_CONFIRMED' && status !== 'RESERVATION_REFUSED' && (
+          <Button
+            size='small'
+            color='white'
+            className='text-black-10 w-full'
+            onClick={handleRefuse}
+          >
+            예약 거절
+          </Button>
+        )}
+
+        <Button
+          size='small'
+          color='black'
+          className='text-black-1 w-full'
+          onClick={handleSendMessage}
+        >
+          메시지 보내기
+        </Button>
       </div>
     </div>
   );
