1e2d06882b06026c8357b1c2b75fcc3714ebae8a
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-07 20:57:50 +0900

    feat: 날짜를 포멧팅하는 함수

--- src/utils/formatNumberWithComma.ts
+++ src/utils/formatNumberWithComma.ts
@@ -1,3 +1,13 @@
 export function formatNumberWithComma(value: number): string {
   return new Intl.NumberFormat('ko-KR').format(value);
 }
+
+/**
+ * 날짜를 콤마로 포맷팅하는 함수
+ * @param date 날짜 (YYYY-MM-DD)
+ * @returns 콤마로 포맷팅된 날짜 (ex. 2026.01.01)
+ */
+export function formatDate(date: string): string {
+  const [year, month, day] = date.split('-');
+  return `${Number(year)}.${Number(month)}.${Number(day)}`;
+}
