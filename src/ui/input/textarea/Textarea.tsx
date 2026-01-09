1d868ce4b379c79061a6f2da09a703a3b7cfc7ca
Tnalxmsk <alstnwkd990@naver.com>
2026-01-09 23:51:25 +0900

    feat: Textarea 공통 컴포넌트 추가

---
+++ src/ui/input/textarea/Textarea.tsx
@@ -0,0 +1,19 @@
+import { cn } from '@/utils/cn';
+import { useRef } from 'react';
+
+type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  hasError?: boolean;
+};
+
+export default function Textarea({ hasError, className, ...props }: TextareaProps) {
+  return (
+    <textarea
+      className={cn(
+        'scrollbar-hide bg-black-1 border-black-5 caption-14-rg placeholder:text-black-6 focus:border-black-10 w-full rounded-[0.6rem] border px-[1.2rem] py-[1.1rem]',
+        hasError && 'border-red-500 focus:border-red-500 focus:ring-red-200',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
