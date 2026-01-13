2d8803cc8176cd6ada1aec6f08917311102f9133
민하 <sminha8590@naver.com>
2026-01-13 17:29:38 +0900

    refactor: 불필요 공백 삭제

--- src/app/author-detail/[id]/_section/PortfolioListSection.tsx
+++ src/app/author-detail/[id]/_section/PortfolioListSection.tsx
@@ -12,7 +12,7 @@ export default function PortfolioListSection({ portfolios }: PortfolioListSectio
     <section>
       {isPortfolioEmpty
         ?
-          <div className='flex justify-center items-center min-h-[calc(100vh-29.9rem)] '>
+          <div className='flex justify-center items-center min-h-[calc(100vh-29.9rem)]'>
             <span className='caption-14-rg text-black-6 text-center'>
               아직 작가님이<br/>포트폴리오를 등록하지 않았어요
             </span>
