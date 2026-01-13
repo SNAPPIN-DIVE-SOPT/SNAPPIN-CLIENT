a25fff431e41ceb44d7380115fd8f7304dd866ed
민하 <sminha8590@naver.com>
2026-01-13 16:43:03 +0900

    feat: 상품 목록 섹션 구현

---
+++ src/app/author-detail/[id]/_section/ProductListSection.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import { ProductCard } from '@/ui';
+import { MoodCode } from '@/types/moodCode';
+
+type ProductListSectionProps = {
+  products: {
+    id: number;
+    imageUrl: string;
+    title: string;
+    rate: number;
+    reviewCount: number;
+    photographer: string;
+    price: number;
+    moods: string[];
+  }[]
+}
+
+export default function ProductListSection({ products }: ProductListSectionProps) {
+  const isProductEmpty = products.length === 0;
+
+  return (
+    <section>
+      {isProductEmpty
+        ?
+          <div className='flex justify-center items-center min-h-[calc(100vh-29.9rem)] '>
+            <span className='caption-14-rg text-black-6 text-center'>
+              아직 작가님이<br/>상품을 등록하지 않았어요
+            </span>
+          </div>
+        :
+          <>
+            {products.map((product) => (
+              <Link
+                key={product.id}
+                href={`/product-detail/${product.id}`}
+                className='flex flex-col gap-[1rem] px-[2rem] py-[1.6rem] border-b-1 border-b-black-3 bg-black-1'
+              >
+                <ProductCard
+                  image={{ src: product.imageUrl, alt: `${product.title} 상품 대표 이미자` }}
+                  name={product.title}
+                  rating={product.rate}
+                  reviewCount={product.reviewCount}
+                  author={product.photographer}
+                  price={product.price}
+                  tags={product.moods as MoodCode[]}
+                />
+              </Link>
+            ))}
+          </>
+      }
+    </section>
+  );
+}
