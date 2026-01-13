7073f2d7edd751cc8b9b19cfdca80455d52ba4b2
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-13 20:14:17 +0900

    fix: build error

--- src/app/(auth)/login/callback/page.tsx
+++ src/app/(auth)/login/callback/page.tsx
@@ -1,55 +1,14 @@
-'use client';
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
-import { useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useKakaoLoginMutation } from '../api';
-import { CreateKakaoLoginData } from '@/swagger-api/data-contracts';
-import { setAccessToken } from '@/auth/token';
-import { setUserType } from '@/auth/userType';
-import { USER_TYPES } from '@/auth/constant/userType';
-import { useToast } from '@/ui/toast/hooks/useToast';
+import { Suspense } from 'react';
 import { Loading } from '@/ui';
+import KakaoCallbackPage from './KakaoCallbackPage';
 
-export default function KakaoCallbackPage() {
-  const router = useRouter();
-  const params = useSearchParams();
-  const code = params.get('code');
-  const error = params.get('error');
-  const toast = useToast();
-  const { mutate } = useKakaoLoginMutation()!;
-
-  useEffect(() => {
-    if (error) {
-      console.error('error', error);
-      router.replace('/login?error=kakao');
-      return;
-    }
-    if (!code) return;
-
-    mutate(
-      { code },
-      {
-        onSuccess: (data: CreateKakaoLoginData) => {
-          setAccessToken(data.data?.accessToken ?? '');
-          setUserType(USER_TYPES[0]);
-          router.replace('/');
-        },
-        onError: () => {
-          router.replace('/login?error=kakao');
-          toast.error(
-            '카카오 로그인에 실패했습니다. 잠시 후 다시 시도해주세요.',
-            undefined,
-            'top-[2rem]',
-          );
-        },
-      },
-    );
-  }, [code, error, mutate, router]);
-
+export default function Page() {
   return (
-    <div className='bg-black-10 flex h-dvh flex-col items-center justify-center gap-[1.5rem]'>
-      <Loading />
-      <p className='caption-14-md text-neon-black'>로그인 중...</p>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <KakaoCallbackPage />
+    </Suspense>
   );
 }
