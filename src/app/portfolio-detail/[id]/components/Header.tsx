7aeea966e7852d9456229e60c746186c199c6185
민하 <sminha8590@naver.com>
2026-01-14 20:59:23 +0900

    refactor: 포트폴리오 상세 헤더 border 속성 div -> Navigation으로 위치 변경

--- src/app/portfolio-detail/[id]/components/Header.tsx
+++ src/app/portfolio-detail/[id]/components/Header.tsx
@@ -16,26 +16,22 @@ export default function Header() {
   }
 
   return (
-    <div className='border-b-1 border-b-black-5'>
-      <Navigation
-        left={
-          <IconButton className='w-[2.4rem] h-[2.4rem]' onClick={handleGoBack}>
-            <IconArrowBack />
-          </IconButton>
-        }
-        center={
-          <span className='flex flex-1 text-center caption-14-bd text-black-10'>
-            포트폴리오 상세
-          </span>
-        }
-        right={
-          <IconButton className='w-[2.4rem] h-[2.4rem]' onClick={handleGoHome}>
-            <IconHome />
-          </IconButton>
-        }
-        className='flex justify-between items-center h-[5rem] px-[2rem]'
-        isFixed={true}
-      />
-    </div>
+    <Navigation
+      left={
+        <IconButton className='w-[2.4rem] h-[2.4rem]' onClick={handleGoBack}>
+          <IconArrowBack />
+        </IconButton>
+      }
+      center={
+        <span className='flex flex-1 text-center caption-14-bd text-black-10'>포트폴리오 상세</span>
+      }
+      right={
+        <IconButton className='w-[2.4rem] h-[2.4rem]' onClick={handleGoHome}>
+          <IconHome />
+        </IconButton>
+      }
+      className='flex justify-between items-center h-[5rem] px-[2rem] border-b-1 border-b-black-5'
+      isFixed={true}
+    />
   );
 }
