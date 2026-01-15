c478b27e6fad7a9b91c39f54ffba99bf6e67aa1c
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-15 10:40:53 +0900

    feat: empty view 구현

--- src/app/photographer/(with-layout)/reservation/components/reservation-content/ReservationContent.tsx
+++ src/app/photographer/(with-layout)/reservation/components/reservation-content/ReservationContent.tsx
@@ -6,6 +6,7 @@ import { Divider, SectionTabs } from '@/ui';
 import { RESERVATION_TABS, ReservationTabValue } from '../../constants/tabs';
 import ReservationCard from '../reservation-card/ReservationCard';
 import { RESERVATION_MOCK } from '../../mock/reservation.mock';
+import EmtpyView from '../emtpy-view/EmtpyView';
 
 export default function ReservationContent() {
   const [selectedTabValue, setSelectedTabValue] =
@@ -30,31 +31,38 @@ export default function ReservationContent() {
         </SectionTabs.List>
 
         <SectionTabs.Contents value={selectedTabValue}>
-          <div className='flex flex-col gap-[1.2rem]'>
-            {data.reservations.map((item, index) => {
-              const { reservation } = item;
-              const { product } = reservation;
-              return (
-                <div key={reservation.reservationId}>
-                  <ReservationCard
-                    reservationId={reservation.reservationId}
-                    status={reservation.status as StateCode}
-                    image={{ src: product.imageUrl, alt: product.title }}
-                    name={product.title}
-                    rate={product.rate}
-                    reviewCount={product.reviewCount}
-                    photographer={product.photographer}
-                    price={product.price}
-                    moods={product.moods}
-                    date={reservation.createdAt}
-                  />
-                  {index !== data.reservations.length - 1 && (
-                    <Divider thickness='large' color='bg-black-3' />
-                  )}
-                </div>
-              );
-            })}
-          </div>
+          {data.reservations.length === 0 ? (
+            <EmtpyView
+              title='상품이 없어요'
+              description='‘탐색’에서 다양한 포트폴리오를 확인해보세요'
+            />
+          ) : (
+            <div className='flex flex-col gap-[1.2rem]'>
+              {data.reservations.map((item, index) => {
+                const { reservation } = item;
+                const { product } = reservation;
+                return (
+                  <div key={reservation.reservationId}>
+                    <ReservationCard
+                      reservationId={reservation.reservationId}
+                      status={reservation.status as StateCode}
+                      image={{ src: product.imageUrl, alt: product.title }}
+                      name={product.title}
+                      rate={product.rate}
+                      reviewCount={product.reviewCount}
+                      photographer={product.photographer}
+                      price={product.price}
+                      moods={product.moods}
+                      date={reservation.createdAt}
+                    />
+                    {index !== data.reservations.length - 1 && (
+                      <Divider thickness='large' color='bg-black-3' />
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </SectionTabs.Contents>
       </SectionTabs>
     </div>
