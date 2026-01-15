138699e9103a1d09a7da89807e6e86caeb25f61c
민하 <sminha8590@naver.com>
2026-01-16 02:21:38 +0900

    refactor: 불필요 주석 삭제

--- src/app/product-detail/[productId]/_section/index.ts
+++ src/app/product-detail/[productId]/_section/index.ts
@@ -1,4 +1,3 @@
-// TODO: 순서
 export { default as PhotographerSection } from './PhotographerSection';
 export { default as PortfolioListSection } from './PortfolioListSection';
 export { default as ProductDetailSection } from './ProductDetailSection';
