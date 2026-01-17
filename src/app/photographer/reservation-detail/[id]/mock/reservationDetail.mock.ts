168c7bbd5f03c7bd8fcdf79443a60179a85947ad
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-17 09:21:19 +0900

    fix: build error

--- src/app/photographer/reservation-detail/[id]/mock/reservationDetail.mock.ts
+++ src/app/photographer/reservation-detail/[id]/mock/reservationDetail.mock.ts
@@ -25,5 +25,12 @@ export const RESERVATION_DETAIL_MOCK = {
     extraPrice: 50000,
     totalPrice: 250000,
   },
-  reviewInfo: null,
+  reviewInfo: {
+    id: 1,
+    reviewer: '작성자명',
+    rating: 5,
+    createdAt: '2026-01-01',
+    images: ['https://picsum.photos/576/576?random=1', 'https://picsum.photos/576/576?random=2'],
+    content: '리뷰 내용',
+  },
 };
