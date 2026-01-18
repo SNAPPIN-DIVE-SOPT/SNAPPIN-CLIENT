15346c0e88880ccbfa62e715da115af79603972e
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-18 11:26:01 +0900

    feat: 이미지 base url 등록

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
+      {
+        protocol: 'https',
+        hostname: 'snappin-dev-images.s3.ap-northeast-2.amazonaws.com',
+      }
     ],
   },
 };
