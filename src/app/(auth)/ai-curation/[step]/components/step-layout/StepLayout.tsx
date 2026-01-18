25c590243428365ea41118e0f04d8b308fe01500
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-19 01:29:46 +0900

    refactor: 리뷰 반영

--- src/app/(auth)/ai-curation/[step]/components/step-layout/StepLayout.tsx
+++ src/app/(auth)/ai-curation/[step]/components/step-layout/StepLayout.tsx
@@ -15,7 +15,7 @@ export default function StepLayout({ question, photos }: StepLayoutProps) {
   return (
     <div className='flex flex-col gap-[6.2rem]'>
       <div className='flex flex-col gap-[0.9rem] pt-[3.5rem]'>
-        <h1 className='title-20-bd text-neon-black'>{padNumber(Number(question?.step))}</h1>
+        <h1 className='title-20-bd text-neon-black'>{question?.step ? padNumber(question.step) : ''}</h1>
         <p className='title-20-bd text-black-1'>{question?.contents}</p>
       </div>
       <ImageAnimation images={photos ?? []} />
--- src/app/(auth)/ai-curation/[step]/constants/steps.ts
+++ src/app/(auth)/ai-curation/[step]/constants/steps.ts
@@ -5,8 +5,10 @@ export const TOTAL_STEP_COUNT = 5;
 export const isAiCurationStep = (step: number): boolean => {
   return step >= 1 && step <= TOTAL_STEP_COUNT;
 };
+
 export const getProgress = (step: AiCurationStep) => {
-  return Math.round((step / TOTAL_STEP_COUNT) * 100);
+  const clampedStep = Math.min(Math.max(step, 0), TOTAL_STEP_COUNT);
+  return Math.round((clampedStep / TOTAL_STEP_COUNT) * 100);
 };
 
 
