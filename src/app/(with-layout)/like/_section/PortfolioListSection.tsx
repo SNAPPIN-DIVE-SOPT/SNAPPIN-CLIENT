06c678d154f93776cd56d4ec243cd7f9b31c503e
Tnalxmsk <alstnwkd990@naver.com>
2026-01-19 20:37:44 +0900

    feat: 목록 섹션 SuspenseQuery 적용 및 상태 분리

--- src/app/(with-layout)/like/_section/PortfolioListSection.tsx
+++ src/app/(with-layout)/like/_section/PortfolioListSection.tsx
@@ -1,19 +1,13 @@
-import { PortfolioList, PortfolioListSkeleton } from '@/ui';
+import { PortfolioList } from '@/ui';
 import { useGetLikePortfolios } from '@/app/(with-layout)/like/api';
 import LikeEmpty from '@/app/(with-layout)/like/component/empty/LikeEmpty';
 
 export default function PortfolioListSection() {
-  const { data: likedPortfolios, isPending } = useGetLikePortfolios();
-
-  if (isPending)
-    return (
-      <section className='h-full px-[1rem] py-[1rem]'>
-        <PortfolioListSkeleton />
-      </section>
-    );
+  const { data: likedPortfolios } = useGetLikePortfolios();
 
   if (!likedPortfolios || likedPortfolios.portfolios?.length === 0)
     return <LikeEmpty tab='PORTFOLIO' />;
+
   return (
     <section className='px-[1rem] py-[1rem]'>
       <PortfolioList portfolioList={likedPortfolios.portfolios ?? []} />
