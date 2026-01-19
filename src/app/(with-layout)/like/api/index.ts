fb5b099303a1e547edb600508573cddc40b4dd00
Tnalxmsk <alstnwkd990@naver.com>
2026-01-19 20:05:40 +0900

    feat: 좋아요 상품, 포트폴리오 쿼리키 상수화

--- src/app/(with-layout)/like/api/index.ts
+++ src/app/(with-layout)/like/api/index.ts
@@ -1,16 +1,15 @@
-import {
-  WishedPortfoliosResponse,
-  WishedProductResponse,
-  WishedProductsResponse,
-} from '@/swagger-api/data-contracts';
+import { WishedPortfoliosResponse, WishedProductsResponse } from '@/swagger-api/data-contracts';
 import { useQuery } from '@tanstack/react-query';
-import { apiRequest } from '@/api/apiRequest';   
- import { ApiResponseBodyWishedPortfoliosResponseVoid, ApiResponseBodyWishedProductsResponseVoid } from '@/swagger-api/data-contracts';
-
+import { apiRequest } from '@/api/apiRequest';
+import {
+  ApiResponseBodyWishedPortfoliosResponseVoid,
+  ApiResponseBodyWishedProductsResponseVoid,
+} from '@/swagger-api/data-contracts';
+import { USER_QUERY_KEY } from '@/query-key/user';
 
 export const useGetLikePortfolios = () => {
   return useQuery<WishedPortfoliosResponse>({
-    queryKey: ['likePortfolios'],
+    queryKey: USER_QUERY_KEY.WISHED_PORTFOLIOS(),
     queryFn: async () => {
       const res = await apiRequest<ApiResponseBodyWishedPortfoliosResponseVoid>({
         endPoint: '/api/v1/wishes/portfolios',
@@ -22,11 +21,11 @@ export const useGetLikePortfolios = () => {
   });
 };
 
-export const useGetLikeProducts =  () => {
+export const useGetLikeProducts = () => {
   return useQuery<WishedProductsResponse>({
-    queryKey: ['likeProducts'],
-      queryFn: async () => {
-        const res = await apiRequest<ApiResponseBodyWishedProductsResponseVoid>({
+    queryKey: USER_QUERY_KEY.WISHED_PRODUCTS(),
+    queryFn: async () => {
+      const res = await apiRequest<ApiResponseBodyWishedProductsResponseVoid>({
         endPoint: '/api/v1/wishes/products',
         method: 'GET',
       });
