d26a24386ef00726b5cb740d0cdf7ab91a33e2a3
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-20 16:25:39 +0900

    refactor: prettier 적용

--- src/app/(auth)/ai-curation/[step]/api/index.ts
+++ src/app/(auth)/ai-curation/[step]/api/index.ts
@@ -1,9 +1,14 @@
 'use client';
 
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
 import { apiRequest } from '@/api/apiRequest';
 import { USER_QUERY_KEY } from '@/query-key/user';
-import { ApiResponseBodyGetAllCurationQuestionsResponseVoid, GetCurationQuestionPhotosResponse } from '@/swagger-api/data-contracts';
+import {
+  ApiResponseBodyGetAllCurationQuestionsResponseVoid,
+  GetCurationQuestionPhotosResponse,
+  CreateMoodCurationResponse,
+  ApiResponseBodyCreateMoodCurationResponseVoid,
+} from '@/swagger-api/data-contracts';
 
 export const useGetAiCurationAll = () => {
   return useQuery<GetCurationQuestionPhotosResponse[]>({
@@ -15,7 +20,7 @@ export const useGetAiCurationAll = () => {
       });
 
       if (!res.data?.questions) {
-        throw new Error("Failed to get /api/v1/curation/all");
+        throw new Error('Failed to get /api/v1/curation/all');
       }
 
       return res.data.questions;
@@ -24,13 +29,32 @@ export const useGetAiCurationAll = () => {
 };
 
 export const useGetAiCurationAllPrefetch = () => {
-    const queryClient = useQueryClient();
-  
-    return () => {
-        console.log('prefetch called');
-      queryClient.prefetchQuery({
-        queryKey: USER_QUERY_KEY.AI_CURATION_ALL(),
-        queryFn: useGetAiCurationAll,
-      });
-    }
+  const queryClient = useQueryClient();
+
+  return () => {
+    queryClient.prefetchQuery({
+      queryKey: USER_QUERY_KEY.AI_CURATION_ALL(),
+      queryFn: useGetAiCurationAll,
+    });
   };
+};
+
+export const usePostAiCuration = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation<CreateMoodCurationResponse, Error, number[]>({
+    mutationFn: async (photoIds: number[]) => {
+      const res = await apiRequest<ApiResponseBodyCreateMoodCurationResponseVoid>({
+        endPoint: '/api/v1/curation',
+        method: 'POST',
+        data: { photoIds: photoIds },
+      });
+
+      if (!res.data) throw new Error('No data from POST /api/v1/curation');
+      return res.data;
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(USER_QUERY_KEY.AI_CURATION_RESULT(), data);
+    },
+  });
+};
