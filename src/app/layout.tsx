bc357320303a052c8278e4d0b1b982e1851b76f3
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-01-23 10:32:47 +0900

    feat: favicon & 썸네일

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local';
 import { Providers } from '@/app/providers';
 import '@/styles/global.css';
-import { Viewport } from 'next';
+import { Metadata, Viewport } from 'next';
 
 const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.ttf',
@@ -11,6 +11,36 @@ export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
+};
+
+export const metadata: Metadata = {
+  title: "Snappin'",
+  description: "나만의 무드에서 시작되는 스냅",
+  icons: {
+    icon: "/imgs/favicon.ico",
+  },
+  openGraph: {
+    title: "Snappin'",
+    description: "나만의 무드에서 시작되는 스냅",
+    url: "https://snappin-client.vercel.app/",
+    siteName: "Snappin'",
+    images: [
+      {
+        url: "https://snappin-client.vercel.app/imgs/image-default.png",
+        width: 1200,
+        height: 630,
+        alt: "Snappin' 썸네일",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Snappin'",
+    description: "나만의 무드에서 시작되는 스냅",
+    images: ["https://snappin-client.vercel.app/imgs/image-default.png"],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
