f87f030f1f3426419187dfd222b8037308893561
bhyerain <seunghye0307@naver.com>
2026-01-23 18:53:50 +0900

    refactor: 모달 위치 변동

--- src/app/reservation-detail/[id]/page.client.tsx
+++ src/app/reservation-detail/[id]/page.client.tsx
@@ -49,7 +49,11 @@ export default function PageClient({ reservationId }: ReservationDetailPageClien
         setCancelOpen(false);
       },
       onError: () => {
-        toast.error('예약 취소 중 오류가 발생했습니다. 다시 시도해주세요.');
+        toast.error(
+          '예약 취소 중 오류가 발생했습니다. 다시 시도해주세요.',
+          undefined,
+          'bottom-[8rem]',
+        );
       },
     });
   };
@@ -61,7 +65,11 @@ export default function PageClient({ reservationId }: ReservationDetailPageClien
         setReservationStatus(paymentResponse.status as StateCode);
       },
       onError: () => {
-        toast.error('결제 요청 중 오류가 발생했습니다. 다시 시도해주세요.');
+        toast.error(
+          '결제 요청 중 오류가 발생했습니다. 다시 시도해주세요.',
+          undefined,
+          'bottom-[8rem]',
+        );
       },
     });
   };
