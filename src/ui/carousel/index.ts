3fd495c1fe9c373b3881bd0cbff48a0127ef556d
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-02-10 15:18:57 +0900

    refactor: 사이드 버튼 포함 image carousel 통합

--- src/ui/carousel/index.ts
+++ src/ui/carousel/index.ts
@@ -1,6 +1,7 @@
 export { default as CarouselCuration } from './carousel-curation/CarouselCuration';
 export { default as ImageCarousel } from './image-carousel/ImageCarousel';
 export { default as ImageCarouselWithDots } from './image-carousel/ImageCarouselWithDots';
+export { default as ImageCarouselWithSideBtn } from './image-carousel/ImageCarouselWithSideBtn';
 export {
   Carousel,
   CarouselContent,
