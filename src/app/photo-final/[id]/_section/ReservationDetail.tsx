dd0d98aff60dcdb7dc95e151968a1b9dc2a1125c
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-02-11 15:17:50 +0900

    feat: 클라이언트 예약 상세에 적용

--- src/app/photo-final/[id]/_section/ReservationDetail.tsx
+++ src/app/photo-final/[id]/_section/ReservationDetail.tsx
@@ -1,6 +1,6 @@
 import { StateCode } from '@/types/stateCode';
-import { DetailLayout, DetailRow } from '../components/detail-layout/DetailLayout';
-import { formatCreatedAt, formatReservationDateTime } from '@/utils/formatNumberWithComma';
+import { formatReservationDateTime } from '@/utils/formatNumberWithComma';
+import { Section } from '@/components/layout/reservation/SectionLayout';
 
 type ReservationDetailProps = {
   status: StateCode;
@@ -26,13 +26,17 @@ export default function ReservationDetail({
   const requestNoteText = requestNote === null || requestNote.length === 0 ? '-' : requestNote;
 
   return (
-    <DetailLayout title='예약 상세' subtitle={status}>
-      <span className='caption-10-md text-black-7 mb-[0.4rem]'>{formatCreatedAt(createdAt)}</span>
-      <DetailRow label='날짜 및 시간' value={formatReservationDateTime(date, startTime)} />
-      <DetailRow label='촬영 시간' value={`${durationTime}시간`} />
-      <DetailRow label='촬영 장소' value={place} />
-      <DetailRow label='촬영 인원' value={`${peopleCount}인`} />
-      <DetailRow label='기타 요청 사항' value={requestNoteText} />
-    </DetailLayout>
+    <Section title='예약 상세' stateCode={status}>
+      <Section.Card>
+        <Section.Header createdAt={createdAt} />
+        <Section.Body>
+          <Section.Row label='날짜 및 시간' value={formatReservationDateTime(date, startTime)} />
+          <Section.Row label='촬영 시간' value={`${durationTime}시간`} />
+          <Section.Row label='촬영 장소' value={place} />
+          <Section.Row label='촬영 인원' value={`${peopleCount}인`} />
+          <Section.Row label='기타 요청 사항' value={requestNoteText} />
+        </Section.Body>
+      </Section.Card>
+    </Section>
   );
 }
