9c0819ce12d5d5106289a119084e61cbdeb95e39
bhyerain <seunghye0307@naver.com>
2026-02-13 04:02:52 +0900

    feat: 예약 페이지에 새 섹션탭 적용

--- src/app/(with-layout)/reservations/page.client.tsx
+++ src/app/(with-layout)/reservations/page.client.tsx
@@ -1,49 +1,36 @@
 'use client';
 
-import { SectionTabs } from '@/ui';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import SectionTabsNew from '@/ui/section-tabs-new/SectionTabsNew';
+import { useTabQuerySync } from '@/hooks/useTabQuerySync';
 import { ReservationListSection, ShootCompletedListSection } from './_section';
 import { ClientNavigation } from './components';
 import { RESERVATION_TAB, RESERVATION_TAB_MAP, type ReservationTab } from './constants/tabs';
 
+const RESERVATION_TABS = [
+  { label: RESERVATION_TAB_MAP.CLIENT_OVERVIEW, value: RESERVATION_TAB.CLIENT_OVERVIEW },
+  { label: RESERVATION_TAB_MAP.CLIENT_DONE, value: RESERVATION_TAB.CLIENT_DONE },
+];
+
 export default function PageClient() {
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  const selectedTabValue = (searchParams.get('tab') ??
-    RESERVATION_TAB.CLIENT_OVERVIEW) as ReservationTab;
-
-  const handleTabChange = (value: string) => {
-    const updatedSearchParams = new URLSearchParams(searchParams.toString());
-    updatedSearchParams.set('tab', value);
-    router.push(`${pathname}?${updatedSearchParams.toString()}`);
-  };
+  const { activeValue, handleChange } = useTabQuerySync('tab', RESERVATION_TAB.CLIENT_OVERVIEW);
+  const activeTabValue = activeValue as ReservationTab;
 
   return (
     <div className='bg-black-1 flex min-h-full flex-col'>
-      <ClientNavigation />
-      <SectionTabs value={selectedTabValue} handleValueChange={handleTabChange}>
-        <SectionTabs.List className='bg-black-1 fixed-center fixed z-15'>
-          {/* 예약 현황 */}
-          <SectionTabs.Tab value={RESERVATION_TAB.CLIENT_OVERVIEW}>
-            {RESERVATION_TAB_MAP.CLIENT_OVERVIEW}
-          </SectionTabs.Tab>
+      <header className='sticky top-0 z-100'>
+        <ClientNavigation />
+        <SectionTabsNew
+          tabs={RESERVATION_TABS}
+          activeValue={activeTabValue}
+          onChange={handleChange}
+        />
+      </header>
 
-          {/* 촬영 완료 */}
-          <SectionTabs.Tab value={RESERVATION_TAB.CLIENT_DONE}>
-            {RESERVATION_TAB_MAP.CLIENT_DONE}
-          </SectionTabs.Tab>
-        </SectionTabs.List>
-
-        <SectionTabs.Contents value={RESERVATION_TAB.CLIENT_OVERVIEW} className='mt-[4.5rem]'>
-          <ReservationListSection />
-        </SectionTabs.Contents>
-
-        <SectionTabs.Contents value={RESERVATION_TAB.CLIENT_DONE} className='mt-[4.5rem]'>
-          <ShootCompletedListSection />
-        </SectionTabs.Contents>
-      </SectionTabs>
+      {activeTabValue === RESERVATION_TAB.CLIENT_OVERVIEW ? (
+        <ReservationListSection />
+      ) : (
+        <ShootCompletedListSection />
+      )}
     </div>
   );
 }
