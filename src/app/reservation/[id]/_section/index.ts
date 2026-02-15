63cbfe358579ab5675d39df582c2c18d84134875
bhyerain <seunghye0307@naver.com>
2026-02-16 08:21:46 +0900

    feat: 예약 요청 상품 + 촬영 내역 통합

--- src/app/reservation/[id]/_section/index.ts
+++ src/app/reservation/[id]/_section/index.ts
@@ -2,3 +2,4 @@ export { default as ReservationRequested } from './ReservationRequested';
 export { default as ReservationDetail } from './ReservationDetail';
 export { default as PaymentDetail } from './PaymentDetail';
 export { default as ReviewDetail } from './ReviewDetail';
+export { default as ReservationProduct } from './ReservationProduct';
