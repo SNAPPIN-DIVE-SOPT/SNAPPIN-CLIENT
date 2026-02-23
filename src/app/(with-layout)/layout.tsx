7cb6143d7cf69390c32459369d9371e3531ea0f3
Tnalxmsk <alstnwkd990@naver.com>
2026-02-23 14:11:49 +0900

    refactor: 레이아웃 속성 제거

--- src/app/(with-layout)/layout.tsx
+++ src/app/(with-layout)/layout.tsx
@@ -2,7 +2,7 @@ import { Footer } from '@/ui';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex h-full min-h-0 flex-col'>
+    <div className='flex flex-col'>
       {children}
       <Footer />
     </div>
