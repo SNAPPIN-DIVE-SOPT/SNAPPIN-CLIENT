ffc734b88c191214ab6305faf31d937691de24e2
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-02-26 11:46:27 +0900

    refactor: suspense 경계 분리

--- src/app/photographers/reservation/[id]/page.tsx
+++ src/app/photographers/reservation/[id]/page.tsx
@@ -1,13 +1,10 @@
 import { notFound } from 'next/navigation';
-import {
-  dehydrate,
-  QueryClient,
-  HydrationBoundary,
-  defaultShouldDehydrateQuery,
-} from '@tanstack/react-query';
+import { dehydrate, QueryClient, HydrationBoundary } from '@tanstack/react-query';
+import SectionSkeleton from '@/components/layout/reservation/SectionSkeleton';
 import NavigationClient from './components/navigation-client/Navigation.client';
 import PageClient from './page.client';
 import { prefetchReservationDetail } from './api';
+import { Suspense } from 'react';
 
 type ReservationDetailPageProps = {
   params: Promise<{ id: string }>;
@@ -20,23 +17,24 @@ export default async function Page({ params }: ReservationDetailPageProps) {
     return notFound();
   }
 
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      dehydrate: {
-        shouldDehydrateQuery: (query) =>
-          query.state.status === 'pending' || defaultShouldDehydrateQuery(query),
-      },
-    },
-  });
+  return (
+    <div className='bg-black-3 flex flex-col'>
+      <NavigationClient />
+      <Suspense fallback={<SectionSkeleton />}>
+        <SuspensePage id={id} />
+      </Suspense>
+    </div>
+  );
+}
+
+const SuspensePage = async ({ id }: { id: string }) => {
+  const queryClient = new QueryClient();
 
   await prefetchReservationDetail(queryClient, Number(id));
 
   return (
-    <div className='bg-black-3 flex flex-col'>
-      <NavigationClient />
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <PageClient id={id} />
-      </HydrationBoundary>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <PageClient id={id} />
+    </HydrationBoundary>
   );
-}
+};
