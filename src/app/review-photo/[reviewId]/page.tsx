eb64dc115df8cd86905112b69b670c5f4de3db8c
sminha <sminha8590@naver.com>
2026-02-26 22:32:23 +0900

    refactor: 포토리뷰 토끼리뷰 반영

--- src/app/review-photo/[reviewId]/page.tsx
+++ src/app/review-photo/[reviewId]/page.tsx
@@ -2,6 +2,7 @@ import PageClient from './page.client';
 import NavigationClient from './components/navigation-client/Navigation.client';
 import { notFound } from 'next/navigation';
 import { getUserType } from '@/auth/userType';
+import { USER_TYPE, UserType } from '@/auth/constant/userType';
 
 type ReservationDetailPageProps = {
   params: Promise<{ id: string; reviewId: string }>;
@@ -19,7 +20,7 @@ export default async function Page({ params }: ReservationDetailPageProps) {
   return (
     <div className='bg-black-10 relative flex min-h-dvh flex-col items-center'>
       <NavigationClient />
-      <PageClient reviewId={reviewIdNumber} userType={userType ?? 'CLIENT'} />
+      <PageClient reviewId={reviewIdNumber} userType={userType as UserType ?? USER_TYPE.CLIENT} />
     </div>
   );
 }
