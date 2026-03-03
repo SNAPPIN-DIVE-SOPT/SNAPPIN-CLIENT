5e7ec254852adda5d21b6fb8986d112fc0f331c8
sminha <sminha8590@naver.com>
2026-03-03 20:26:18 +0900

    refactor: 서버 컴포넌트로 통합

--- src/app/photographer/[id]/page.tsx
+++ src/app/photographer/[id]/page.tsx
@@ -1,4 +1,14 @@
-import ClientPage from './page.client';
+import { Suspense } from 'react';
+import { ROUTES } from '@/constants/routes/routes';
+import { Tabs, PortfolioListSkeleton, ProductListSkeleton } from '@/ui';
+import {
+  PhotographerSection,
+  PortfolioListSection,
+  ProductListSection,
+} from './_section/index';
+import { Header, Footer } from './components/index';
+import { PHOTOGRAPHER_TAB, PHOTOGRAPHER_TABS } from './constants/tab';
+import { PhotographerSectionSkeleton } from './_section/index';
 
 type PageProps = {
   params: Promise<{ id: string }>;
@@ -9,6 +19,53 @@ export default async function Page({ params, searchParams }: PageProps) {
   const { id } = await params;
   const { tab } = await searchParams;
   const photographerId = Number(id);
+  const selectedTab = tab ?? PHOTOGRAPHER_TAB.PORTFOLIO;
 
-  return <ClientPage id={photographerId} tab={tab} />;
+  return (
+    <main className='flex flex-col'>
+      <Header />
+      <Suspense fallback={<PhotographerSectionSkeleton />}>
+        <PhotographerSection id={photographerId} />
+      </Suspense>
+      <Tabs>
+        <Tabs.List activeValue={selectedTab} tabs={PHOTOGRAPHER_TABS} className='bg-black-1 fixed top-[17.6rem] z-10 w-full max-w-[45rem] px-[2rem]'>
+          {PHOTOGRAPHER_TABS.map(({ value, label }) => (
+            <Tabs.Item
+              key={value}
+              value={value}
+              activeValue={selectedTab}
+              href={ROUTES.PHOTOGRAPHER(photographerId, { tab: value })}
+            >
+              {label}
+            </Tabs.Item>
+          ))}
+        </Tabs.List>
+        <div>
+          {selectedTab === PHOTOGRAPHER_TAB.PORTFOLIO && (
+            <div className='bg-black-1 mb-[7.6rem] p-[1rem]'>
+              <Suspense fallback={
+                <div className='mt-[17.1rem]'>
+                  <PortfolioListSkeleton />
+                </div>
+              }>
+                <PortfolioListSection id={photographerId} />
+              </Suspense>
+            </div>
+          )}
+          {selectedTab === PHOTOGRAPHER_TAB.PRODUCT && (
+            <div className='mb-[7.6rem]'>
+              <Suspense fallback={
+                <div className='mt-[17.1rem]'>
+                  <ProductListSkeleton />
+                </div>
+              }>
+                <ProductListSection id={photographerId} />
+              </Suspense>
+            </div>
+          )}
+        </div>
+      </Tabs>
+      <Footer />
+    </main>
+  );
 }
