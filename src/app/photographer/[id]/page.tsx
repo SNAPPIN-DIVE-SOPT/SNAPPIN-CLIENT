73bb175a2d706a88c772acd91a7f9c4b8efa4438
sminha <sminha8590@naver.com>
2026-03-03 18:50:26 +0900

    refactor: SectionTabs -> Tabs

--- src/app/photographer/[id]/page.tsx
+++ src/app/photographer/[id]/page.tsx
@@ -2,10 +2,12 @@ import ClientPage from './page.client';
 
 type PageProps = {
   params: Promise<{ id: string }>;
+  searchParams: Promise<{ tab: string }>;
 }
 
-export default async function Page({ params }: PageProps) {
+export default async function Page({ params, searchParams }: PageProps) {
   const { id } = await params;
+  const { tab } = await searchParams;
   const photographerId = Number(id);
-  return <ClientPage id={photographerId} />;
+  return <ClientPage id={photographerId} tab={tab} />;
 }
