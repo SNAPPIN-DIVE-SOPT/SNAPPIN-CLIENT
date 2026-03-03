4606b7cca2edfdd3efa2ff33381c55182f9ce3c1
sminha <sminha8590@naver.com>
2026-03-04 00:30:14 +0900

    refactor: 쿼리 옵션 분리

--- src/app/product/[id]/api/server.ts
+++ src/app/product/[id]/api/server.ts
@@ -1,60 +1,23 @@
 import { QueryClient } from '@tanstack/react-query';
-import { SERVER_API_BASE_URL } from '@/api/constants/api';
-import { USER_QUERY_KEY } from '@/query-key/user';
-import { getProductDetail} from './index';
+import { productDetailOptions, productPortfoliosOptions, productReviewsOptions } from './options';
 
+// 상품 상세 정보 및 상품 안내 조회 prefetch
 export const prefetchProductDetail = (queryClient: QueryClient, id: number, isLogIn: boolean) => {
-  return queryClient.prefetchQuery({
-    queryKey: USER_QUERY_KEY.PRODUCT_DETAIL(id, isLogIn),
-    queryFn: () => getProductDetail(id, isLogIn),
-  })
-}
+  return queryClient.prefetchQuery(productDetailOptions(id, isLogIn));
+};
 
+// 포폴 목록 조회 prefetch
 export const prefetchPortfolioList = async (queryClient: QueryClient, id: number) => {
   return queryClient.prefetchInfiniteQuery({
-    queryKey: USER_QUERY_KEY.PRODUCT_PORTFOLIOS(id),
-    initialPageParam: undefined, 
-    queryFn: async ({ pageParam }) => {
-      const url = new URL(`${SERVER_API_BASE_URL}/api/v1/portfolios`);
-      url.searchParams.append('productId', String(id));
-      if (pageParam) {
-        url.searchParams.append('cursor', String(pageParam));
-      }
-
-      const res = await fetch(url.toString(), { method: 'GET' });
-
-      if (!res.ok) {
-        throw new Error('/api/v1/portfolios 응답에 데이터가 존재하지 않습니다.');
-      }
-
-      return await res.json();
-    },
-    getNextPageParam: (lastPage) => 
-      lastPage.meta?.hasNext ? lastPage.meta.nextCursor : undefined,
+    ...productPortfoliosOptions(id),
     pages: 1,
   });
 };
 
+// 상품 리뷰 목록 조회 prefetch
 export const prefetchProductReviewList = async (queryClient: QueryClient, id: number) => {
   return queryClient.prefetchInfiniteQuery({
-    queryKey: USER_QUERY_KEY.PRODUCT_REVIEWS(id),
-    initialPageParam: undefined,
-    queryFn: async ({ pageParam }) => {
-      const url = new URL(`${SERVER_API_BASE_URL}/api/v1/products/${id}/reviews`);
-      if (pageParam) {
-        url.searchParams.append('cursor', String(pageParam));
-      }
-
-      const res = await fetch(url.toString(), { method: 'GET' });
-
-      if (!res.ok) {
-        throw new Error('/api/v1/products/{productId}/reviews 응답에 데이터가 존재하지 않습니다.');
-      }
-
-      return await res.json();
-    },
-    getNextPageParam: (lastPage) => 
-      lastPage.meta?.hasNext ? lastPage.meta.nextCursor : undefined,
+    ...productReviewsOptions(id),
     pages: 1,
   });
 };
