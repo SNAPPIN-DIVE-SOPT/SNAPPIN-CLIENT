d5d6b1773b65ade09ba5a649989c7bb47b252f12
Tnalxmsk <alstnwkd990@naver.com>
2026-03-06 18:12:24 +0900

    chore(packages/prettier-config): add shared prettier config package

---
+++ packages/prettier-config/index.mjs
@@ -0,0 +1,11 @@
+export default {
+  "singleQuote": true,
+  "jsxSingleQuote": true,
+  "trailingComma": "all",
+  "semi": true,
+  "printWidth": 100,
+  "arrowParens": "always",
+  "bracketSpacing": true,
+  "endOfLine": "lf",
+  "plugins": ["prettier-plugin-tailwindcss"]
+}
