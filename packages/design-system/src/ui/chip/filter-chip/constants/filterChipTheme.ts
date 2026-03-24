1fd3a03935482f2de046136299cd0f8bab8a0cc4
Tnalxmsk <alstnwkd990@naver.com>
2026-03-24 16:04:01 +0900

    feat(web): 필터 칩 요구사항 변경 반영

--- packages/design-system/src/ui/chip/filter-chip/constants/filterChipTheme.ts
+++ packages/design-system/src/ui/chip/filter-chip/constants/filterChipTheme.ts
@@ -3,8 +3,11 @@ import { FilterChipVariant } from '../types/filterChipVariant';
 type FilterChipTheme = { buttonClassName: string; labelClassName: string };
 
 export const FILTER_CHIP_THEME: Record<FilterChipVariant, FilterChipTheme> = {
-  default: { buttonClassName: 'px-[0.8rem] bg-black-3', labelClassName: 'text-black-8' },
-  selected: { buttonClassName: 'px-[0.8rem] bg-black-10', labelClassName: 'text-black-1' },
+  default: {
+    buttonClassName: 'px-[1.2rem] bg-black-1 border-[0.1rem] rounded-[2.4rem] border-black-4',
+    labelClassName: 'text-black-7 ',
+  },
+  selected: { buttonClassName: 'px-[1.2rem] bg-black-10 rounded-[3.4rem]', labelClassName: 'text-black-1' },
   selectedRemovable: {
     buttonClassName: 'h-[2.6rem] pl-[0.8rem] pr-[0.3rem]',
     labelClassName: 'text-black-1 cursor-default',
