2fc299032059e9e1524837aa4afba1db911a6074
Tnalxmsk <alstnwkd990@naver.com>
2026-03-26 21:13:24 +0900

    feat(web): 프로필 컴포넌트 스토리 수정

--- apps/web/src/ui/profile/Profile.stories.tsx
+++ apps/web/src/ui/profile/Profile.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import Profile from './Profile';
-import { IconArrowForward } from '@snappin/design-system/assets';
 
 const meta: Meta<typeof Profile> = {
   title: 'UI/ProfileItem',
@@ -12,7 +11,7 @@ export default meta;
 
 type Story = StoryObj<typeof Profile>;
 
-export const ShortcutCard: Story = {
+export const Default: Story = {
   render: () => (
     <div className='border-black-9 w-[32rem] border-[0.1rem]'>
       <Profile size='sm'>
@@ -25,39 +24,6 @@ export const ShortcutCard: Story = {
             </Profile.Description>
           </Profile.Item>
         </Profile.Content>
-      </Profile>
-    </div>
-  ),
-};
-
-export const CompoundInline: Story = {
-  render: () => (
-    <div className='border-black-9 w-[32rem] border-[0.1rem]'>
-      <Profile size='sm'>
-        <Profile.Avatar src='/imgs/default-profile.png' />
-        <Profile.Content lines={2}>
-          <Profile.Item>
-            <Profile.Title>나작가</Profile.Title>
-            <Profile.Description typography='caption-12-rg' color='black-8'>
-              내일의 나를 기록합니다
-            </Profile.Description>
-          </Profile.Item>
-          <Profile.Item>
-            <Profile.Row>
-              <Profile.Meta className='shrink-0'>촬영 상품</Profile.Meta>
-              <Profile.Meta typography='caption-12-rg' color='black-8'>
-                졸업스냅, 웨딩스냅, 일상스냅
-              </Profile.Meta>
-            </Profile.Row>
-            <Profile.Row>
-              <Profile.Meta className='shrink-0'>활동 지역</Profile.Meta>
-              <Profile.Meta typography='caption-12-rg' color='black-8'>
-                서울
-              </Profile.Meta>
-            </Profile.Row>
-          </Profile.Item>
-        </Profile.Content>
-        <Profile.Trailing />
       </Profile>
     </div>
   ),
@@ -89,66 +55,6 @@ export const CompoundCard: Story = {
           </Profile.Item>
         </Profile.Content>
         <Profile.Trailing />
-      </Profile>
-    </div>
-  ),
-};
-
-export const ShortcutSingleLine: Story = {
-  render: () => (
-    <div className='border-black-9 w-[32rem] border-[0.1rem]'>
-      <Profile>
-        <Profile.Avatar src='/imgs/default-profile.png' />
-        <Profile.Content lines={1}>
-          <Profile.Item>
-            <Profile.Title>나작가</Profile.Title>
-            <Profile.Description typography='caption-12-rg' color='black-8'>
-              서울
-            </Profile.Description>
-          </Profile.Item>
-        </Profile.Content>
-      </Profile>
-    </div>
-  ),
-};
-
-export const AsChildAnchor: Story = {
-  render: () => (
-    <div className='border-black-9 w-[32rem] border-[0.1rem]'>
-      <a href='/profile' className='block'>
-        <Profile>
-          <Profile.Avatar src='/imgs/default-profile.png' />
-          <Profile.Content lines={1}>
-            <Profile.Item>
-              <Profile.Title>링크형 프로필</Profile.Title>
-              <Profile.Description typography='caption-12-rg' color='black-8'>
-                바로가기
-              </Profile.Description>
-            </Profile.Item>
-          </Profile.Content>
-          <Profile.Trailing />
-        </Profile>
-      </a>
-    </div>
-  ),
-};
-
-export const CustomTrailingIcon: Story = {
-  render: () => (
-    <div className='border-black-9 w-[37.5rem] border-[0.1rem]'>
-      <Profile>
-        <Profile.Avatar size='md' src='/imgs/default-profile.png' />
-        <Profile.Content lines={1}>
-          <Profile.Item>
-            <Profile.Title>나작가</Profile.Title>
-            <Profile.Description typography='caption-12-rg' color='black-8'>
-              서울
-            </Profile.Description>
-          </Profile.Item>
-        </Profile.Content>
-        <Profile.Trailing>
-          <IconArrowForward className='text-black-6 h-[2.4rem] w-[2.4rem]' />
-        </Profile.Trailing>
       </Profile>
     </div>
   ),
