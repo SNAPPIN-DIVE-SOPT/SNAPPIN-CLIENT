5660df775e4419bd0fcd6e814be7f776f62c9a3f
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-04-02 12:54:48 +0900

    feat(web): step 1일 시 back btn 노출 x

--- apps/web/src/app/(auth)/on-boarding/[step]/components/client-navigation/ClientNavigation.tsx
+++ apps/web/src/app/(auth)/on-boarding/[step]/components/client-navigation/ClientNavigation.tsx
@@ -1,15 +1,22 @@
 ﻿'use client';
 
-import { useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { IconArrowBack } from '@snappin/design-system/assets';
 import { Navigation } from '@snappin/design-system';
 
 export default function ClientNavigation() {
   const router = useRouter();
+  const params = useParams<{ step: string }>();
+  const isFirstStep = params.step === '1';
 
   const handleBackClick = () => {
     router.back();
   };
 
-  return <Navigation isFixed={true} left={<IconArrowBack onClick={handleBackClick} />} />;
+  return (
+    <Navigation
+      isFixed={true}
+      left={isFirstStep ? undefined : <IconArrowBack onClick={handleBackClick} />}
+    />
+  );
 }
