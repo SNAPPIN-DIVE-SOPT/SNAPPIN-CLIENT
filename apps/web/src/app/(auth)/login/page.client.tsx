5cdf323cf65a28aa49c700160b28546a3ce3c004
hamxxn <150767569+hamxxn@users.noreply.github.com>
2026-04-19 11:23:25 +0900

    refactor(web): login 서버, 클라이언트 페이지 분리

---
+++ apps/web/src/app/(auth)/login/page.client.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Logo } from '@snappin/design-system/assets';
+import { ImageSlide } from '@snappin/design-system';
+import useLoginPage from '@/app/(auth)/login/hooks/useLoginPage';
+import { ClientNavigation, LoginButton } from '@/app/(auth)/login/components';
+
+type PageClientProps = {
+  error?: string;
+  returnTo?: string;
+};
+
+export default function PageClient({ error, returnTo }: PageClientProps) {
+  const { handleKakaoLogin } = useLoginPage({
+    loginError: error,
+    returnTo,
+  });
+
+  return (
+    <>
+      <ClientNavigation />
+      <div className='mt-[6.4rem] flex flex-col gap-[2.4rem]'>
+        <div className='flex flex-col items-center gap-[1.2rem]'>
+          <Logo className='text-neon-black h-[2.8rem] w-[15.8rem]' />
+          <p className='title-20-md text-black-1'>나만의 무드에서 시작되는 스냅</p>
+        </div>
+        <ImageSlide />
+        <div className='flex justify-center px-[2rem]'>
+          <LoginButton onKakaoLogin={handleKakaoLogin} />
+        </div>
+      </div>
+    </>
+  );
+}
--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,7 +1,5 @@
-﻿import { Logo } from '@snappin/design-system/assets';
-import { ImageSlide } from '@snappin/design-system';
-import { readReturnToContext } from '@/auth/utils/returnTo';
-import { ClientNavigation, LoginButton } from './components';
+﻿import { readReturnToContext } from '@/auth/utils/returnTo';
+import { LoginPageContent } from './components';
 
 type PageProps = {
   searchParams: Promise<{
@@ -20,17 +18,7 @@ export default async function Page({ searchParams }: PageProps) {
 
   return (
     <div className='bg-black-10 h-full'>
-      <ClientNavigation />
-      <div className='mt-[6.4rem] flex flex-col gap-[2.4rem]'>
-        <div className='flex flex-col items-center gap-[1.2rem]'>
-          <Logo className='text-neon-black h-[2.8rem] w-[15.8rem]' />
-          <p className='title-20-md text-black-1'>나만의 무드에서 시작되는 스냅</p>
-        </div>
-        <ImageSlide />
-        <div className='flex justify-center px-[2rem]'>
-          <LoginButton returnTo={returnTo} loginError={error} />
-        </div>
-      </div>
+      <LoginPageContent error={error} returnTo={returnTo} />
     </div>
   );
 }
